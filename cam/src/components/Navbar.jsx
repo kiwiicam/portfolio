1e1fa77ab99f2f806a0437afc1cbf1c96bd7a48e
kiwiicam <mastercamnz@gmail.com>
2024-11-06 20:33:20 +1300

    change of style and started about page

--- cam/src/components/Navbar.jsx
+++ cam/src/components/Navbar.jsx
@@ -8,46 +8,30 @@ import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 
 function Navbar() {
-    const [open, setOpen] = useState(false);
-    function showMenu() {
-        setOpen(prevState => !prevState);
-    }
     return (
         <>
             <div className='container'>
 
-                {open ?
+                <div className='box'>
 
-                    <div className='container-cross'>
-
-                        <AiOutlineClose className='cross' onClick={showMenu} />
-                        <div className='box'>
-
-                            <Link to="">
-                                <h2>About me</h2>
-                            </Link>
-                            <Link to="">
-                                <h2>Experience</h2>
-                            </Link>
-                            <Link to="">
-                                <h2>Projects</h2>
-                            </Link>
-                            <Link to="">
-                                <h2>Contact me</h2>
-                            </Link>
-                        </div>
-
+                    <div className='nav'>
+                        <h2>About</h2>
                     </div>
-                    :
-                    <div className='dropDown' >
-                        <IoMenu className='menu' onClick={showMenu} />
-                        <h2>Menu</h2>
+                    <div className='nav'>
+                        <h2>Experience</h2>
                     </div>
-                }
+                    <div className='nav'>
+                        <h2>Projects</h2>
+                    </div>
+                    <div className='nav'>
+                        <h2>Contact</h2>
+                    </div>
+                </div>
+
                 <div className='end-icons'>
-                    <MdOutlineMail className='mail'/>
-                    <CiLinkedin className='linked-in'/>
-                    <FaGithub className='github'/>
+                    <MdOutlineMail className='mail' />
+                    <CiLinkedin className='linked-in' />
+                    <FaGithub className='github' />
                 </div>
             </div>
         </>
--- cam/src/pages/HomePage.jsx
+++ cam/src/pages/HomePage.jsx
@@ -1,30 +1,64 @@
 import React from 'react'
 import '../css/HomePage.css'
 import Typewriter from 'typewriter-effect';
+import { Link, animateScroll as scroll } from 'react-scroll';
+import { MdOutlinePerson4 } from "react-icons/md";
+
 function HomePage() {
   return (
     <>
       <div className='main'>
-        <Typewriter
-          options={{
-            autoStart: true,
-            loop: true,
-            wrapperClassName: 'typing',
-            cursorClassName: 'cursor',
-          }}
+        <div className='text'>
+          <h2>
+            Hi, I'm
+          </h2>
+          <div className='red'><h2>Campbell</h2></div>
+        </div>
+        <div className='type'>
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              wrapperClassName: 'typing',
+              cursorClassName: 'cursor',
+            }}
 
-          onInit={(typewriter) => {
-            typewriter.typeString('Welcome to my portfolio!')
-              .pauseFor(2500)
-              .deleteAll()
-              .pauseFor(2000)
-              .start();
-          }}
-        />
+            onInit={(typewriter) => {
+              typewriter.typeString('A Computer Science Student')
+                .pauseFor(1500)
+                .deleteAll()
+                .pauseFor(1000)
+              typewriter.typeString('A Great learner')
+                .pauseFor(2500)
+                .deleteAll()
+                .pauseFor(1000)
+              typewriter.typeString('A Hard Worker')
+                .pauseFor(2500)
+                .deleteAll()
+                .pauseFor(1000)
+                .start();
+            }}
+          />
+
+        </div>
+        <button className='proj-button' onClick={() => scroll.scrollTo(850)}>Projects</button>
 
       </div>
       <div className='about'>
-          about me
+        <h2>About</h2>
+        <div className='underline'>
+        </div>
+        <div className='underline2'>
+        </div>
+        <div className='writing'>
+          <div className='left-div'>
+            <MdOutlinePerson4 className='person' />
+            <p className='left'>I am a hardworking student studying Computer Science at the Auckland University of Technology, I am majoring in Software Development with a minor in Artificial Intelligence, I am in my 2nd year. I enjoy developing web applications, low-level operations and regular software</p>
+          </div>
+          <div className='right-div'>
+            <p className='right'>My skills include: C - used to understand lower-level topics such as operating systems, Java - used as a multipurpose language and my go-to for coding problems, CSS, HTML, JavaScript, React - my web framework of choice and Node/Express JS - for my backend work</p>
+          </div>
+        </div>
       </div>
     </>
   )
